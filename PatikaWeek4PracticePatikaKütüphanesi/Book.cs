fa0fac1d0a24af5efd50090be9ae92aec18c256b
Bilal <bilalbilin12@gmail.com>
2024-08-06 15:58:48 +0300

    Add project files.

---
+++ PatikaWeek4PracticePatikaKütüphanesi/Book.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PracticePatikaKütüphanesi
+{
+    
+    public class Book // Creating a new class
+    {
+        private string _bookName;
+        private string _authorName;
+        private string _authorSurname;
+        private int _pageCount;
+        private string _publisher;
+        private DateTime _registrationDate;
+
+        // Setting properties to access and modify the fields given above
+        public string bookName 
+        {
+            get
+            {
+                return _bookName;
+            }
+            set
+            {
+                _bookName = value;
+            }
+        }
+        public string authorName
+        {
+            get
+            {
+                return _authorName;
+            }
+            set
+            {
+                _authorName = value;
+            }
+        }
+
+        public string authorSurname
+        {
+            get
+            {
+                return _authorSurname;
+            }
+            set
+            {
+                _authorSurname = value;
+            }
+        }
+
+        public int pageCount
+        {
+            get
+            {
+                return _pageCount;
+            }
+            set
+            {
+                _pageCount = value;
+            }
+        }
+
+        public string publisher
+        {
+            get
+            {
+                return _publisher;
+            }
+            set
+            {
+                _publisher = value;
+            }
+        }
+
+        public DateTime registrationDate
+        {
+            get
+            {
+                return _registrationDate;
+            }
+            set
+            {
+                _registrationDate = value;
+            }
+        }
+
+
+
+
+        public Book() // Default constructor
+        {
+            RegistrationDate();
+        }
+
+        public Book(string bookName, string authorName, string authorSurname, int pageCount, string publisher) // Constructor that takes parameters
+        {
+            _bookName = bookName;
+            _authorName = authorName;
+            _authorSurname = authorSurname;
+            _pageCount = pageCount;
+            _publisher = publisher;
+            RegistrationDate();
+        }
+
+        public void BookInfo() // Method that prints out information about books.
+        {
+            Console.WriteLine($"{_authorName} {_authorSurname} kişisine ait {_pageCount} sayfalık, {_publisher} yayınevinden çıkan {_bookName} adlı kitap oluşturuldu.");
+            Console.WriteLine($"Kayıt tarihi: {_registrationDate}");
+        }
+
+        public void RegistrationDate() // Method to save current date and time when its called
+        {
+            _registrationDate = DateTime.Now;
+        }
+    }
+}
---
+++ PatikaWeek4PracticePatikaKütüphanesi/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using PatikaWeek4PracticePatikaKütüphanesi;
+
+// Creating a new object using default constructor
+
+Book book1 = new Book(); 
+book1.bookName = "Bilal'in Sancıları";
+book1.authorName = "Bilal";
+book1.authorSurname = "Bilin";
+book1.pageCount = 1;
+book1.publisher = "Patika";
+
+book1.BookInfo();
+
+Console.WriteLine("---------------------------------------------------------");
+
+// Creating a new object using constructor that takes parameters
+
+Book book2 = new Book("Kalecinin Penaltı Anındaki Endişesi", "Peter", "Handke", 112, "Ayrıntı");
+
+book2.BookInfo();
+
+Console.WriteLine("---------------------------------------------------------");
+
+Book book3 = new Book("Türklük Sözleşmesi", "Barış", "Ünlü", 380, "Dipnot");
+
+book3.BookInfo();
+
+
+
+/*
+
+    Class: Belirli bir türdeki nesnelerin özelliklerini ve davranışlarını belirleyen bir şablondur. Verileri(Properties ve fields) ve bu verilere yönelik işlemleri(methods) barındırır.
+    
+    Property: Sınıf içerisindeki field'lara erişimi kontrol eden ve sınıfın dışına belirli bir düzeyde güvenli erişim sağlayan yapılar olarak tanımlanabilir.
+    
+    New: OOP'de bir sınıfın örneğini yani nesnesini(object) oluşturmak için kullanılan anahtar sözcüktür.
+    
+    Constructor: Sınıf yapılarının nesne olarak tanımlanmasında kurucu olan, sınıf ismiyle aynı isime sahip olan, geriye değer döndürmeyen bir metod türüdür.
+ 
+ 
+ */
